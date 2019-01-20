18083dc2026cf4a994faa30475269899869465b8
Elorolf <98biqueton10>
2019-01-20 07:08:02 +0100

    Officiel

--- update matth/includes/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$stmt = mysqli_stmt_init($conn);
-mysqli_stmt_execute($stmt);
-$result = mysqli_stmt_get_result($stmt);
-$row = mysqli_fetch_assoc($result);
-$_SESSION['idHouse'] = $row['idHouse'];
-
-?>
